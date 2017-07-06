91b6524302377e0aa1ed0c17884b07a5d97434c9
root <root@ubuntu-512mb-nyc3-01.localdomain>
2017-07-06 08:17:37 +0000

    add new model for user services

---
+++ app/UserservicesModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\UserservicesModel;
+
+
+class UserservicesModel extends Model
+{
+
+    protected $table = "user_services";
+    
+    public $timestamps = false;
+
+    public static function get_services_id($service_id)
+    {
+	/*Get record based on the parameter*/
+        $get_services_id = UserservicesModel::where("id",$service_id)->first();
+
+        /*if record exist return record*/
+        if($get_services_id)
+        {
+            return $get_services_id;
+        }else{
+
+            return false;
+        }
+    }
+
+    public static function update_radius_services_id($radius_services_id,$radius_service_id)
+    {
+	
+	$update_services_id = UserservicesModel::where("id",$radius_services_id)->update(['radius_service_id'=>$radius_service_id]);
+
+	return $update_services_id;
+
+
+    }
+
+   public static function update_radius($fields)
+   {
+	
+	/*Filter the array data and get only data elements with values*/
+	$fields =  array_filter($fields);
+	
+	/*itemporary static ip for now and update record base on the on the fields inputs*/
+	$update_services = UserservicesModel::where("id",'31826')->update($fields);
+	return $update_services;
+   }
+
+   
+}
+
